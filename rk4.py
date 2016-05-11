67ef7e119ab66669ea49dab35bd54773b8656022
tri <tri@THINK>
2016-05-11 15:25:47 +0200

    first commit

---
+++ rk4.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+
+""" rk4.py
+
+    Runge - Kutta 4th order method of the Roessler System
+
+    Copyright 2016 Tri-Peter Shrive
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Tri-Peter Shrive
+    Anklamer Str 13
+    10115 Berlin
+    Deutschland
+
+    +49 17 62087558
+    Tri.Shrive@gmail.com
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import multiprocessing as mp
+
+def rk4(Dx, Dy, Dz, a):
+	x0, y0, z0, b, c, t0, Tt, n = return_values()
+	vx = [0] * (n + 1)
+	vy = [0] * (n + 1)
+	vz = [0] * (n + 1)
+	vt = [0] * (n + 1)
+
+	h  = (Tt - t0) / float(n)
+
+	vx[0] = x = x0
+	vy[0] = y = y0
+	vz[0] = z = z0
+
+	for i in range(1, n + 1):
+		k1_Dx, k1_Dy, k1_Dz = Dx(y, z), Dy(x, y, a), Dz(x, z, b, c)
+
+		k2_Dx, k2_Dy, k2_Dz = Dx(y + h * 0.5 * k1_Dy, z + h * 0.5 * k1_Dz), Dy(x + h * 0.5 * k1_Dx, y + h * 0.5 * k1_Dy, a), Dz(x + h * 0.5 * k1_Dx, z + h * 0.5 * k1_Dz, b, c)
+
+		k3_Dx, k3_Dy, k3_Dz = Dx(y + h * 0.5 * k2_Dy, z + h * 0.5 * k2_Dz), Dy(x + h * 0.5 * k2_Dx, y + h * 0.5 * k2_Dy, a), Dz(x + h * 0.5 * k2_Dx, z + h * 0.5 * k2_Dz, b, c)
+
+		k4_Dx, k4_Dy, k4_Dz = Dx(y + h * k3_Dy, z + h * k3_Dz), Dy(x + h * k3_Dx, y + h * k3_Dy, a), Dz(x + h * k3_Dx, z + h * k3_Dz, b, c)
+		
+		vt[i] = t = t0 + i * h
+		vx[i] = x = x + h * (k1_Dx + k2_Dx + k2_Dx + k3_Dx + k3_Dx + k4_Dx) / 6
+		vy[i] = y = y + h * (k1_Dy + k2_Dy + k2_Dy + k3_Dy + k3_Dy + k4_Dy) / 6
+		vz[i] = z = z + h * (k1_Dz + k2_Dz + k2_Dz + k3_Dz + k3_Dz + k4_Dz) / 6
+
+	return vx, vy, vz, vt
+
+def Dx(y, z):
+	return - y - z
+
+def Dy(x, y, a):
+	return x + a * y
+
+def Dz(x, z, b, c):
+	return b + z * (x - c)
+
+def overtime(vx, vy, vz, vt, t0, Tt):
+
+	fig = plt.figure(1)
+	fig.suptitle('Roessler System over Time', fontsize=12, fontweight='bold')
+
+	ax = fig.add_subplot(311)
+	ax.set_ylabel('Dx')
+	plt.plot( vt, vx, 'r' )
+	plt.xlim(t0,Tt)
+
+	ay = fig.add_subplot(312)
+	ay.set_ylabel('Dy')
+	plt.plot( vt, vy, 'r' )
+	plt.xlim(t0,Tt)
+
+	az = fig.add_subplot(313)
+	az.set_ylabel('Dz')
+	az.set_xlabel('Dt')
+	plt.plot( vt, vz, 'r' )
+	plt.xlim(t0,Tt)
+
+	plt.show()
+	return
+
+def graph3d(vx, vy, vz, vt):
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	fig.suptitle('Roessler System over Time', fontsize=12, fontweight='bold')
+	ax.set_ylabel('Dx')
+	ax.set_xlabel('Dy')
+	ax.set_zlabel('Dz')
+	plt.plot( vx, vy, vz, 'r' )
+	plt.show()
+	return
+
+def bifurcation_worker(a):
+	"""worker function for calculating the tragectories of the henon map"""
+	vx, vy, vz, vt = rk4(Dx, Dy, Dz, a)
+	return vx[-20:]
+
+def bifurcation_pool(a0, a1, a_n):
+	with mp.Pool(processes=mp.cpu_count()-1) as pool:
+		T = pool.map(bifurcation_worker, np.arange(a0, a1, a_n))
+
+	plt.plot(np.arange(a0, a1, a_n), T, 'r.', markersize = 0.65)
+	plt.xlim([a0,a1])
+
+	plt.show()
+
+	return
+
+def return_values():
+	x0, y0, z0 = 1, 0, 0
+	b, c = 2, 4
+
+	t0 , Tt = 0, 55
+	n =  100 * Tt # RK4 step size
+
+	return x0, y0, z0, b, c, t0, Tt, n
+
+def main():
+	x0, y0, z0, b, c, t0, Tt, n = return_values()
+	a = 0.5
+	a0, a1, a_n = 0.2, 0.55, 0.0001 # a_n - Bifurcation step size
+
+	vx, vy, vz, vt = rk4(Dx, Dy, Dz, a)
+#	for x, y, z, t in list(zip(vx, vy, vz, vt))[::1]:
+#		print("%1.1f %1.1f %1.1f %1.1f" % (x, y, z, t))
+
+	
+
+	overtime(vx, vy, vz, vt, t0, Tt)
+
+	graph3d(vx, vy, vz, vt)
+
+	bifurcation_pool(a0, a1, a_n)
+
+
+main()
+
